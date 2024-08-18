4866d0450dff3d911aefad7af6f91bda0df5d7bf
Karol Hetman <karol.hetman@protonmail.com>
2024-08-18 23:57:36 +0200

    yes

--- src/main.rs
+++ src/main.rs
@@ -30,6 +30,7 @@ fn place_player(
                 .insert(TransformBundle::from(Transform::from_xyz(0.0, -100.0, 0.0)))
                 .insert(RigidBody::KinematicPositionBased)
                 .insert(Collider::ball(100.0 / 2.0))
+                .insert(LockedAxes::TRANSLATION_LOCKED)
                 .insert(KinematicCharacterController::default());
         
         commands
@@ -46,36 +47,37 @@ fn place_player(
 
 fn player_movement(
     keyboard_input: Res<ButtonInput<KeyCode>>,
-    mut player_query: Query<&mut KinematicCharacterController, With<Player>>,
+    mut player_query: Query<(&mut KinematicCharacterController, &mut Transform), With<Player>>,           
     speed_query: Query<&Speed, With<Player>>,
     time: Res<Time>,
 ) {
-    if let Ok(mut transform) = player_query.get_single_mut() {
+    if let Ok((mut controller, mut transform)) = player_query.get_single_mut() {
         let mut direction = Vec3::ZERO;
+        let mut rotation_direction = 0.0;
 
-        if keyboard_input.pressed(KeyCode::ArrowLeft) || keyboard_input.pressed(KeyCode::KeyA) {
-            direction += Vec3::new(-1.0, 0.0, 0.0);
-        }
-        if keyboard_input.pressed(KeyCode::ArrowRight) || keyboard_input.pressed(KeyCode::KeyD) {
-            direction += Vec3::new(1.0, 0.0, 0.0);
-        }
-        if keyboard_input.pressed(KeyCode::ArrowUp) || keyboard_input.pressed(KeyCode::KeyW) {
-            direction += Vec3::new(0.0, 1.0, 0.0);
-        }
-        if keyboard_input.pressed(KeyCode::ArrowDown) || keyboard_input.pressed(KeyCode::KeyS) {
-            direction += Vec3::new(0.0, -1.0, 0.0);
+        for key in keyboard_input.get_pressed() {
+            match key {
+                KeyCode::ArrowLeft | KeyCode::KeyA => direction += Vec3::new(-1.0, 0.0, 0.0),
+                KeyCode::ArrowRight | KeyCode::KeyD => direction += Vec3::new(1.0, 0.0, 0.0),
+                KeyCode::ArrowUp | KeyCode::KeyW => direction += Vec3::new(0.0, 1.0, 0.0),
+                KeyCode::ArrowDown | KeyCode::KeyS => direction += Vec3::new(0.0, -1.0, 0.0),
+                KeyCode::KeyZ => rotation_direction += 1.0,
+                KeyCode::KeyX => rotation_direction -= 1.0,
+                _ => (),
+            }
         }
 
         if direction.length() > 0.0 {
             direction = direction.normalize();
         }
+
         let speed = speed_query.get_single().unwrap().0;
         let movement = Some(Vec2::new(direction.x, direction.y) * speed * time.delta_seconds());
 
-        transform.translation = movement;
+        controller.translation = movement;
+        transform.rotation *= Quat::from_rotation_z(rotation_direction * 5.0 * time.delta_seconds());
     }
 }
-
 
 fn crosshair_tracking(
     mut params: ParamSet<(
