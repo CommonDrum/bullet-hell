263f79b2dc1832dbf6045273848f051a9b57fb47
Karol Hetman <karol.hetman@protonmail.com>
2024-09-16 23:28:56 +0100

    round raycast

--- src/utils.rs
+++ src/utils.rs
@@ -37,3 +37,26 @@ pub fn angle_between_points(point1: Vec2, point2: Vec2) -> f32 {
     let difference = point2 - point1;
     difference.y.atan2(difference.x)
 }
+
+pub fn round_raycast(
+    rapier_context: &RapierContext,
+    entity_position: Vec2,
+    arr_size: usize,
+    ray_length: f32,
+    solid: bool,
+    filter: QueryFilter,
+) -> Vec<bool> {
+    let mut hit_results = vec![false; arr_size];
+
+    for i in 0..arr_size {
+        let angle = index_to_radians(i as usize, arr_size);
+        let ray_dir = Vec2::new(angle.cos(), angle.sin());
+
+        if let Some(_) = rapier_context.cast_ray(entity_position, ray_dir, ray_length, solid, filter) {
+            hit_results[i] = true;
+        }
+    }
+
+    hit_results
+}
+
